666b53983df1f44b2d326921d2ec2bd4921d7d93
DmitriyKalganovv <142721999+DmitriyKalganovv@users.noreply.github.com>
2024-01-28 18:08:29 +0500

    mew_game

---
+++ tg.js
@@ -0,0 +1,5 @@
+const bot = new Telegraf("6574674839:AAF5JutJJT-7qo4kd2IrfUVGt_lMH3QuIME");
+
+bot.sendMessage("Открыть сайт в Telegram", {
+    url: "https://dmitriykalganovv.github.io/my-html5-game/"
+});
